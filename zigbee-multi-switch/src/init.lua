59862057f66148ab0e6612b144b21e17cf9821f0
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2021-10-05 15:00:48 +0200

    Add files via upload
    Delete code not used

--- zigbee-multi-switch/src/init.lua
+++ zigbee-multi-switch/src/init.lua
@@ -20,20 +20,36 @@ local zcl_clusters = require "st.zigbee.zcl.clusters"
 local OnOff = zcl_clusters.OnOff
 local data_types = require "st.zigbee.data_types"
 
+local ep_ini = 1
+
+--- return endpoint from component_id
 local function component_to_endpoint(device, component_id)
   if component_id == "main" then
+    ep_ini = device.fingerprinted_endpoint_id
     return device.fingerprinted_endpoint_id
   else
     local ep_num = component_id:match("switch(%d)")
-    return ep_num and tonumber(ep_num) or device.fingerprinted_endpoint_id
+    if ep_num == "2" then
+      return ep_ini + 1
+     --return ep_num and tonumber(ep_num) or device.fingerprinted_endpoint_id
+    elseif ep_num == "3" then
+      return ep_ini + 2
+    end
   end
 end
 
+--- return Component_id from endpoint
 local function endpoint_to_component(device, ep)
   if ep == device.fingerprinted_endpoint_id then
+    ep_ini = ep
     return "main"
   else
-    return string.format("switch%d", ep)
+    if ep == ep_ini + 1 then
+      --return string.format("switch%d", ep)
+      return "switch2"
+    elseif ep == ep_ini + 2 then
+      return "switch3"
+    end 
   end
 end
 
@@ -42,87 +58,13 @@ local function device_init (self, device)
   device:set_component_to_endpoint_fn(component_to_endpoint)
   device:set_endpoint_to_component_fn(endpoint_to_component)
   -- Refresh schedule
-  device.thread:call_on_schedule(
-    300,
-    function ()
-      local refresh = device:refresh ()
-      --return refresh
-    end,
-    'Refresh schedule') 
+  --device.thread:call_on_schedule(
+    --300,
+    --function ()
+      --local refresh = device:refresh ()
+    --end,
+    --'Refresh schedule') 
 end
-
---- Command on handler ----
-local function on_handler(_, device, command)
-  -- capability reference
-  local attr = capabilities.switch.switch
-  -- parse component to endpoint
-  local endpoint = device:get_endpoint_for_component_id(command.component)
-  -- send zigbee event
-  device:send(OnOff.server.commands.On(device):to_endpoint(endpoint))
-  -- send platform event
-  device:emit_event_for_endpoint(endpoint, attr.on())
-end
-
---- Command off handler ----
-local function off_handler(_, device, command)
-  -- capability reference
-  local attr = capabilities.switch.switch
-  -- parse component to endpoint
-  local endpoint = device:get_endpoint_for_component_id(command.component)    
-  -- send zigbee event
-  device:send(OnOff.server.commands.Off(device):to_endpoint(endpoint))
-  -- send platform event
-  device:emit_event_for_endpoint(endpoint, attr.off())
-end
-
---- read zigbee attribute OnOff messages and detect ON pushbutton pressed in device ----
-local function on_off_attr_handler(self, device, value, zb_rx)
-  print ("function: on_off_attr_handler")
-  local src_endpoint = zb_rx.address_header.src_endpoint.value
-  local attr_value = value.value
-  if src_endpoint == 1 then
-    device:send(zcl_clusters.OnOff.attributes.OnOff:read(device):to_endpoint (2))
-    device:send(zcl_clusters.OnOff.attributes.OnOff:read(device):to_endpoint (3))
-    if attr_value == false then
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.off())
-    else
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.on())
-    end
-  end
-  if src_endpoint == 2 then
-    if attr_value == false then
-      device:send(OnOff.server.commands.Off(device):to_endpoint(src_endpoint))
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.off())
-    else
-      device:send(OnOff.server.commands.On(device):to_endpoint(src_endpoint))
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.on())
-    end
-  end
-  if src_endpoint == 3 then
-    if attr_value == false then
-      device:send(OnOff.server.commands.Off(device):to_endpoint(src_endpoint))
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.off())
-    else
-      device:send(OnOff.server.commands.On(device):to_endpoint(src_endpoint))
-      device:emit_event_for_endpoint(src_endpoint, capabilities.switch.switch.on())
-    end
-  end
-  print ("src_endpoint , value:", zb_rx.address_header.src_endpoint.value, value.value)
-end
-
------ Configur device -------
---local function do_Configure (self, device)
-  --device:send(device_management.build_bind_request(device, zcl_clusters.OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint (1))
-  --device:send(zcl_clusters.OnOff.attributes.OnOff:configure_reporting(device, 0, 300):to_endpoint (1))
-  --device:send(device_management.build_bind_request(device, zcl_clusters.OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint (2))
-  --device:send(zcl_clusters.OnOff.attributes.OnOff:configure_reporting(device, 0, 300):to_endpoint (2))
-  --device:send(device_management.build_bind_request(device, zcl_clusters.OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint (3))
-  --device:send(zcl_clusters.OnOff.attributes.OnOff:configure_reporting(device, 0, 300):to_endpoint (3))
-  --device:configure()
-  --device:emit_event_for_endpoint(1, capabilities.switch.switch.off())
-  --device:emit_event_for_endpoint(2, capabilities.switch.switch.off())
-  --device:emit_event_for_endpoint(3, capabilities.switch.switch.off())
---end
 
 ---- Driver configure ---------
 local zigbee_outlet_driver_template = {
@@ -132,22 +74,7 @@ local zigbee_outlet_driver_template = {
   },
   lifecycle_handlers = {
     init = device_init,
-    --doConfigure = do_Configure,
   },
-  --zigbee_handlers = {
-      --attr = {
-        --[zcl_clusters.OnOff.ID] = {
-           --[zcl_clusters.OnOff.attributes.OnOff.ID] = on_off_attr_handler
-       --}
-     --}
-  --},
-  --capability_handlers = {
-    --[capabilities.switch.ID] = {
-      --[capabilities.switch.commands.on.NAME] = on_handler,
-      --[capabilities.switch.commands.off.NAME] = off_handler
-    --},
-  --},
-  --sub_drivers = { require("lidl") }
 }
 
 defaults.register_for_default_handlers(zigbee_outlet_driver_template, zigbee_outlet_driver_template.supported_capabilities)
