892e6a3232fca8249ae7311c80ebde27e08f50bc
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2022-10-26 17:30:15 +0200

    Add files via upload
    Added virtual child thermostat device

---
+++ zigbee-temp-humidity-child-thermostat/src/battery/init.lua
@@ -0,0 +1,58 @@
+local zcl_clusters = require "st.zigbee.zcl.clusters"
+local capabilities = require "st.capabilities"
+local battery = capabilities.battery
+local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+local utils = require "st.utils"
+
+local can_handle = function(opts, driver, device)
+  if device.manufacturer ~= nil then return false end
+    if device:get_manufacturer() == "_TZ2000_a476raq2" then
+      return device:get_manufacturer() == "_TZ2000_a476raq2"
+    end
+end
+
+local battery_handler = function(driver, device, value, zb_rx)
+-- Emit Battery voltage event
+   --device:emit_event(capabilities.voltageMeasurement.voltage(value.value / 10))
+  
+   if device:get_manufacturer() == "SmartThings" then
+
+    local batteryMap = {[28] = 100, [27] = 100, [26] = 100, [25] = 90, [24] = 90, [23] = 70,
+                      [22] = 70, [21] = 50, [20] = 50, [19] = 30, [18] = 30, [17] = 15, [16] = 1, [15] = 0}
+    local minVolts = 15
+    local maxVolts = 28
+
+    value = utils.clamp_value(value.value, minVolts, maxVolts)
+
+    device:emit_event(battery.battery(batteryMap[value]))
+  else
+    local minVolts = 2.3
+    local maxVolts = 3.0
+
+    local battery_pct = math.floor(((((value.value / 10) - minVolts) + 0.001) / (maxVolts - minVolts)) * 100)
+    if battery_pct > 100 then 
+      battery_pct = 100
+     elseif battery_pct < 0 then
+      battery_pct = 0
+     end
+    device:emit_event(battery.battery(battery_pct))
+  end
+end
+
+local battery_voltage = {
+	NAME = "battery_voltage",
+    zigbee_handlers = {
+        attr = {
+            [zcl_clusters.PowerConfiguration.ID] = {
+              [zcl_clusters.PowerConfiguration.attributes.BatteryVoltage.ID] = battery_handler
+            }
+        }
+    },
+    lifecycle_handlers = {
+        --init = battery_defaults.build_linear_voltage_init(2.3, 3.0)
+        added = battery_defaults.build_linear_voltage_init(2.3, 3.0)
+    },
+	can_handle = can_handle
+}
+
+return battery_voltage
---
+++ zigbee-temp-humidity-child-thermostat/src/child-devices.lua
@@ -0,0 +1,49 @@
+-- Module to create child LAN device
+-- M. Colmenarejo 2022
+
+
+local child_devices = {}
+
+-- Global variable type table to save all child devices (device variable value) created indexed by Parent device ID (phisical device) and component name: Child_devices_created[device.parent_device_id .. component] = device
+Child_devices_created = {}
+-- Global variable type table to save all Parent devices (device variable value) that have child devices created, indexed by Parent device ID (phisical device): Parent_devices[device.id] = device
+Parent_devices = {}
+
+-- Create child device
+function child_devices.create_new(driver, device, component)
+
+    local label = "Thermostat-".. device.label
+    local profile = "child-thermostat"
+
+    if Child_devices_created[device.id .. "-Thermostat"] == nil then
+        -- save device parent table
+        if Parent_devices[device.id] == nil then 
+          Parent_devices[device.id] = device
+        end
+        print("Parent_devices[" .. device.id .."]>>>>>>", Parent_devices[device.id])
+
+        local metadata = {
+            type = "LAN",
+            device_network_id = "Thermostat-" .. os.time(), -- DNI for Child device 
+            label = label,                              -- Initial Label for Child device
+            profile = profile,                          -- Profile assigned to Child device created
+            parent_device_id = device.id,               -- used to save parent device ID
+            manufacturer = "child-device",              -- used to save type of device (need to identify if message come from Child or Parent Device)
+            model = component,                          -- used to save component name of the parent device
+            vendor_provided_label = device.label        -- used to save parent label it if need
+        }
+        
+        -- Create new device
+        driver:try_create_device(metadata)
+
+        --print("device_network_id >>>",device.device_network_id)
+        --print("label >>>",device.label)
+        --print("parent_device_id >>>",device.parent_device_id)
+        --print("manufacturer >>>",device.manufacturer)
+        --print("model >>>",device.model)
+        --print("vendor_provided_label >>>",device.vendor_provided_label)
+      end
+
+end
+
+  return child_devices
