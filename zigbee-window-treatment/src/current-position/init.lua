9f1c346bd47238f6da682da469d02e22fb7830bc
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2023-08-19 13:42:24 +0200

    Update init.lua

--- zigbee-window-treatment/src/current-position/init.lua
+++ zigbee-window-treatment/src/current-position/init.lua
@@ -1,4 +1,6 @@
 -- Copyright 2022 SmartThings
+-- 
+-- this module created by M.Colmenarejo
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
