68a6243a683eb869cd7a4a48470c16273e713f75
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2024-09-01 11:48:10 +0200

    Add fingerprints

--- zigbee-smoke-co-detector/src/gas-handler/init.lua
+++ zigbee-smoke-co-detector/src/gas-handler/init.lua
@@ -24,6 +24,7 @@ local configurationMap = require "configurations"
 local is_gas_detector = function(opts, driver, device)
   if device.network_type ~= "DEVICE_EDGE_CHILD" then -- is NO CHILD DEVICE
     if (device:get_manufacturer() == "LUMI" and device:get_model() == "lumi.sensor_gas.acn02") or
+      (device:get_manufacturer() == "feibit" and device:get_model() == "FNB56-GAS05FB1.4") or
       (device:get_manufacturer() == "_TYZB01_mfccmeio" and device:get_model() == "TS0204") then -- gas detector
       --or (device:get_manufacturer() == "_TYZB01_18pkine6" and device:get_model() == "TS0204")
       local subdriver = require("gas-handler")
