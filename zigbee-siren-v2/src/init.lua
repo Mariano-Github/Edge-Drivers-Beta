ebecadd6fba3c92821c4f6fc6be2aa33b325944e
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2025-11-09 12:14:22 +0100

    Updated to firmware 59.x

--- zigbee-siren-v2/src/init.lua
+++ zigbee-siren-v2/src/init.lua
@@ -226,6 +226,13 @@ local function do_preferences (self, device, event, args)
 end
 
 -- this new function in libraries version 9 allow load only subdrivers with devices paired
+local version = require "version"
+local lazy_handler
+if version.api >= 15 then
+  lazy_handler = require "st.utils.lazy_handler"
+else
+  lazy_handler = require
+end
 local function lazy_load_if_possible(sub_driver_name)
   -- gets the current lua libs api version
   local version = require "version"
@@ -294,7 +301,8 @@ local zigbee_siren_driver_template = {
         data_type = IASZone.attributes.ZoneStatus.base_type
       }
     }
-  }
+  },
+  health_check = false
 }
 
 defaults.register_for_default_handlers(zigbee_siren_driver_template, zigbee_siren_driver_template.supported_capabilities)
